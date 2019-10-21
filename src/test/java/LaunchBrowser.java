cd5dfeab93834dcfba07fa65aabadefb8627679a
Shahnawaz <shahnawaz.sayyed2015@outlook.com>
2019-10-21 17:54:18 +0530

    Set Browser as Chrome

--- src/test/java/LaunchBrowser.java
+++ src/test/java/LaunchBrowser.java
@@ -13,12 +13,11 @@ public class LaunchBrowser {
         System.setProperty("webdriver.chrome.driver",DriverPath + "chromedriver.exe");
         System.setProperty("webdriver.gecko.driver",DriverPath + "geckodriver.exe");
         System.setProperty("webdriver.edge.driver",DriverPath + "msedgedriver.exe");
-        //WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         //WebDriver driver = new FirefoxDriver();
-        WebDriver driver = new EdgeDriver();
+        //WebDriver driver = new EdgeDriver();
 
         driver.get("https://phptravels.com/demo/");
-        //driver.get("https://phptravels.com/demo/");
 
         driver.close();
     }
