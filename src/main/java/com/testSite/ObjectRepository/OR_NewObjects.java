de84d5694dd9f81ffd3b5547574f5bc2a8c09aae
Shahnawaz <shahnawaz.sayyed2015@outlook.com>
2020-09-26 21:25:01 +0530

    Another Update to OR

--- src/main/java/com/testSite/ObjectRepository/OR_NewObjects.java
+++ src/main/java/com/testSite/ObjectRepository/OR_NewObjects.java
@@ -3,7 +3,7 @@ package com.testSite.ObjectRepository;
 import org.openqa.selenium.By;
 
 public class OR_NewObjects {
-
-    public static By TXT_EMAIL_ID = By.xpath("//*[@id='email']");
-
+    
+    Pass
+    
 }
